e94407309eadb07dbb60cccb7a094a89248e6abd
Aman Prakash <33799434+aman30865@users.noreply.github.com>
2020-06-07 19:04:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -22,6 +22,16 @@ def predict():
 
     return render_template('index.html', prediction_text='Employee Salary should be $ {}'.format(output))
 
-
+@app.route('/upload')  
+def upload():  
+    return render_template("file_upload_form.html")  
+ 
+@app.route('/success', methods = ['POST'])  
+def success():  
+    if request.method == 'POST':  
+        f = request.files['file']  
+        f.save(f.filename)  
+        return render_template("success.html", name = f.filename)  
+    
 if __name__ == "__main__":
     app.run(debug=True)
